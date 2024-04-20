f6cfc942bb16df0e0a9b03a3e13c14d1291823d2
ringsaturn <ringsaturn.me@gmail.com>
2024-04-20 17:07:54 +0800

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![deny(clippy::all)]
+// #![deny(clippy::all)]
 
 use lazy_static::lazy_static;
 use napi_derive::napi;
@@ -7,11 +7,6 @@ use tzf_rs::DefaultFinder;
 lazy_static! {
   static ref FINDER: DefaultFinder = DefaultFinder::default();
 }
-
-// #[napi]
-// pub fn plus_100(input: u32) -> u32 {
-//   input + 100
-// }
 
 #[napi]
 pub fn get_tz(lng: f64, lat: f64) -> String {
